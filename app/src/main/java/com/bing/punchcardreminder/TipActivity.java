4cf3cf6ba86f83b9a4987b3bfd50b4edd981cfeb
贺兵 <hebing0305@gmail.com>
2019-08-30 16:34:51 +0800

    2.0稳定版 不出意外应该不会再更新了

--- app/src/main/java/com/bing/punchcardreminder/TipActivity.java
+++ app/src/main/java/com/bing/punchcardreminder/TipActivity.java
@@ -3,7 +3,6 @@ package com.bing.punchcardreminder;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
-import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +12,10 @@ public class TipActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+//    getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     setContentView(R.layout.activity_tip);
     vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
     vibrator.vibrate(new long[]{500, 500, 500, 500, 500, 500, 500, 500, 500}, 1);
-//    vibrator.vibrate(VibrationEffect.createWaveform());
   }
 
   @Override
--- app/src/main/java/com/bing/punchcardreminder/receiver/AlarmReceiver.java
+++ app/src/main/java/com/bing/punchcardreminder/receiver/AlarmReceiver.java
@@ -3,6 +3,7 @@ package com.bing.punchcardreminder.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -13,12 +14,15 @@ public class AlarmReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
     System.out.println("AlarmReceiver onReceive");
-    Intent mIntent = context.getPackageManager().getLaunchIntentForPackage("com.alibaba.android.rimet");
-    if (mIntent != null) {
-      mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      context.startActivity(mIntent);
+    SharedPreferences sp = context.getSharedPreferences("WIFI", Context.MODE_PRIVATE);
+    boolean isOpenDingDing = sp.getBoolean(AppUtils.DING_DING_SWITCH, false);
+    if (isOpenDingDing) {
+      Intent mIntent = context.getPackageManager().getLaunchIntentForPackage("com.alibaba.android.rimet");
+      if (mIntent != null) {
+        mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(mIntent);
+      }
+      AppUtils.nextAlarm(context);
     }
-
-    AppUtils.nextAlarm(context);
   }
 }
